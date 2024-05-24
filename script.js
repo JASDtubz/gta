4c4f40ca1a446663e17eb3b71fcdcfb9fe52a8b9
JASD <86506578+JASDtubz@users.noreply.github.com>
2024-05-23 23:24:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,89 @@
-/* Version 2023.10.04.22.28 */
+/* Version 2024.0523.2323:0051 */
 
-class Song {
-  contructor(name, info, link) {
-    this.name = name;
-    this.info = info;
-    this.link = link;
-  }
-}
+var intros = [
+  { name: "My Ordinay Life<br>Frist Intro", info: "Top tier", link: "https://jasdtubz.github.io/gta/op/my_ordinary_life_1.wav" },
+  { name: "To Love Ru", info: "Top tier", link: "https://jasdtubz.github.io/gta/op/to_love_ru.wav" }
+];
 
 var outros = [
-  new Song("Nichijou (All Version Mashup)", "Top tier", "ed/nichijou_ultimate.wav")
+  { name: "Miss Kobayashi's Dragon Maid", info: "Great", link: "https://jasdtubz.github.io/gta/ed/miss_kobayashis_dragon_maid.wav" },
+  { name: "Nichijou (All Version Mashup)", info: "Top tier", link: "https://jasdtubz.github.io/gta/ed/nichijou_ultimate.wav" }
 ];
 
 var currenttrack;
+var op = null;
+var opindex = 0, edindex = 0;
 
 function init() {
+  intros = randomizearray(intros);
   outros = randomizearray(outros);
 }
 
 function opstart() {
   document.getElementById("homewrap").style.display = "none";
   document.getElementById("playwrap").style.display = "initial";
+  
+  op = true;
+  currenttrack = playsound(intros[opindex].link);
 }
 
 function edstart() {
   document.getElementById("homewrap").style.display = "none";
   document.getElementById("playwrap").style.display = "initial";
   
-  currenttrack = playsound(outros.link);
+  op = false;
+  currenttrack = playsound(outros[edindex].link);
 }
 
 function home() {
   document.getElementById("playwrap").style.display = "none";
   document.getElementById("homewrap").style.display = "initial";
+  
+  op = null;
+  reset();
+}
+
+function reveal() {
+  document.getElementById("reveal").style.fontSize = "25px";
+  
+  if (op) {
+    document.getElementById("reveal").innerHTML = intros[opindex].name;
+  } else {
+    document.getElementById("reveal").innerHTML = outros[edindex].name;
+  }
+}
+
+function prev() {
+  if (op) {
+    if (opindex != 0) {
+      reset();
+      currenttrack = playsound(intros[--opindex].link);
+    }
+  } else {
+    if (edindex != 0) {
+      reset();
+      currenttrack = playsound(outros[--edindex].link);
+    }
+  }
+}
+
+function next() {
+  if (op) {
+    if (opindex != intros.length - 1) {
+      reset();
+      currenttrack = playsound(intros[++opindex].link);
+    }
+  } else {
+    if (edindex != outros.length - 1) {
+      reset();
+      currenttrack = playsound(outros[++edindex].link);
+    }
+  }
+}
+
+function reset() {
+  document.getElementById("reveal").style.fontSize = "40px";
+  document.getElementById("reveal").innerHTML = "Reveal";
   currenttrack.pause();
 }
 
@@ -63,5 +114,5 @@ function removeitem(array, item) {
 }
 
 function randomint(min, max) {
-  Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
