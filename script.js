d048a2a32f822284195dffb93d209230bb988867
JASD <86506578+JASDtubz@users.noreply.github.com>
2024-05-29 00:01:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-/* Version 2024.0523.2323:0051 */
+/* Version 2024.0529.0000:0051 */
 
 var intros = [
-  { name: "My Ordinay Life<br>Frist Intro", info: "Top tier", link: "https://jasdtubz.github.io/gta/op/my_ordinary_life_1.wav" },
-  { name: "To Love Ru", info: "Top tier", link: "https://jasdtubz.github.io/gta/op/to_love_ru.wav" }
+  { name: "My Ordinay Life<br>Frist Intro", info: "Tier S", link: "https://jasdtubz.github.io/gta/op/my_ordinary_life_1.wav", reveal: false},
+  { name: "My Senpai Is Annoying", info: "Tier A", link: "https://jasdtubz.github.io/gta/op/my_senpai_is_annoying.wav", reveal: false},
+  { name: "To Love Ru", info: "Tier S", link: "https://jasdtubz.github.io/gta/op/to_love_ru.wav", reveal: false }
 ];
 
 var outros = [
-  { name: "Miss Kobayashi's Dragon Maid", info: "Great", link: "https://jasdtubz.github.io/gta/ed/miss_kobayashis_dragon_maid.wav" },
-  { name: "Nichijou (All Version Mashup)", info: "Top tier", link: "https://jasdtubz.github.io/gta/ed/nichijou_ultimate.wav" }
+  //{ name: "Nichijou (All Version Mashup)", info: "Tier S", link: "https://jasdtubz.github.io/gta/ed/nichijou_ultimate.wav", reveal: false },
+  { name: "Made In Abyss", info: "Tier A", link: "https://jasdtubz.github.io/gta/ed/made_in_abyss.wav", reveal: false}, //Make my own
+  { name: "Miss Kobayashi's Dragon Maid", info: "Tier A", link: "https://jasdtubz.github.io/gta/ed/miss_kobayashis_dragon_maid.wav", reveal: false },
+  { name: "My Senpai Is Annoying", info: "Tier B", link: "https://jasdtubz.github.io/gta/ed/my_senpai_is_annoying.wav", reveal: false },
 ];
+
+// artist namem title, my opion
 
 var currenttrack;
 var op = null;
@@ -44,25 +49,37 @@ function home() {
 }
 
 function reveal() {
-  document.getElementById("reveal").style.fontSize = "25px";
-  
   if (op) {
-    document.getElementById("reveal").innerHTML = intros[opindex].name;
+    if (!intros[opindex].reveal) {
+      document.getElementById("reveal").innerHTML = intros[opindex].name;
+      intros[opindex].reveal = true;
+    } else {
+      document.getElementById("reveal").innerHTML = "Reveal";
+      intros[opindex].reveal = false;
+    }
   } else {
-    document.getElementById("reveal").innerHTML = outros[edindex].name;
+    if (!outros[edindex].reveal) {
+      document.getElementById("reveal").innerHTML = outros[edindex].name;
+      outros[edindex].reveal = true;
+    } else {
+      document.getElementById("reveal").innerHTML = "Reveal";
+      outros[edindex].reveal = false;
+    }
   }
 }
 
 function prev() {
   if (op) {
     if (opindex != 0) {
+      --opindex;
       reset();
-      currenttrack = playsound(intros[--opindex].link);
+      currenttrack = playsound(intros[opindex].link);
     }
   } else {
     if (edindex != 0) {
+      --edindex;
       reset();
-      currenttrack = playsound(outros[--edindex].link);
+      currenttrack = playsound(outros[edindex].link);
     }
   }
 }
@@ -70,20 +87,34 @@ function prev() {
 function next() {
   if (op) {
     if (opindex != intros.length - 1) {
+      ++opindex;
       reset();
-      currenttrack = playsound(intros[++opindex].link);
+      currenttrack = playsound(intros[opindex].link);
     }
   } else {
     if (edindex != outros.length - 1) {
+      ++edindex;
       reset();
-      currenttrack = playsound(outros[++edindex].link);
+      currenttrack = playsound(outros[edindex].link);
     }
   }
 }
 
 function reset() {
-  document.getElementById("reveal").style.fontSize = "40px";
-  document.getElementById("reveal").innerHTML = "Reveal";
+  if (op) {
+    if (!intros[opindex].reveal) {
+      document.getElementById("reveal").innerHTML = "Reveal";
+    } else {
+      document.getElementById("reveal").innerHTML = intros[opindex].name;
+    }
+  } else {
+    if (!outros[edindex].reveal) {
+      document.getElementById("reveal").innerHTML = "Reveal";
+    } else {
+      document.getElementById("reveal").innerHTML = outros[edindex].name;
+    }
+  }
+  
   currenttrack.pause();
 }
 
